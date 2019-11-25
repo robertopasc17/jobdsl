36fc5322dd6db7f35ff4690eaef857c084b6eba7
root <root@ip-172-31-23-92.eu-west-3.compute.internal>
2019-11-25 22:56:36 +0000

    modificado python.groovy

--- jobs/python.groovy
+++ jobs/python.groovy
@@ -1,3 +1,10 @@
+folder('project-a') {
+    displayName('Project A')
+    description('Folder for project A')
+}
+
+
+
 project_name = "jenkins-pipeline-python"
 repo = "git@github.com:robertopasc17/jenkins.git"
 repo_name = "repo"
