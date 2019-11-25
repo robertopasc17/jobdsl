ad8ad93a52635d396ead06d129902f11123f2d1e
root <root@ip-172-31-23-92.eu-west-3.compute.internal>
2019-11-25 22:47:25 +0000

    añadido test.groovy

---
+++ jobs/test.groovy
@@ -0,0 +1,25 @@
+project_name = "jenkins-pipeline-python"
+repo = "git@github.com:robertopasc17/jenkins.git"
+repo_name = "repo"
+
+
+pipelineJob(project_name) {
+	definition {
+		triggers{
+			scm('H/1 * * * *')
+	}
+
+	cpsScm{
+		scm {
+			git {
+				remote {
+				name(repo_name)
+				url(repo)
+				}
+			}
+			scriptPath("Jenkinsfile")
+
+			}
+		}
+	}
+}
